5d21988c5a304fe560ca880510a4ef26de803532
vibedivide <x@sys32.dev>
2021-06-22 21:28:53 -0400

    bump version for release

--- sploit/meta.js
+++ sploit/meta.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'Sploit',
 	author: 'The Gaming Gurus',
 	description: 'Powerful Krunker mod',
-	version: '1.6.4',
+	version: '1.6.5',
 	license: 'gpl-3.0',
 	namespace: 'https://e9x.github.io/',
 	icon: 'https://y9x.github.io/webpack/libs/gg.gif?',
