e0ee34fa02dbf6312dafcc01ac441e373cae8515
Daniel Reed <x@sys32.dev>
2021-12-28 17:00:48 -0500

    Update console.js

--- libs/console.js
+++ libs/console.js
@@ -1,5 +1,5 @@
 'use strict';
 
-for(let prop of ['log','warn','error','trace','table','debug','group','groupCollapsed','groupEnd']){
+for(let prop of ['log','warn','info','error','trace','table','debug','group','groupCollapsed','groupEnd']){
 	exports[prop] = console[prop].bind(console);
 }
