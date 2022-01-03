3afbcf2533828c0ce7d222b375c335b1eca7c1e8
Daniel Reed <x@sys32.dev>
2022-01-02 23:59:21 -0500

    GPL

--- loader/meta.js
+++ loader/meta.js
@@ -4,6 +4,7 @@ module.exports = {
 	name: `Krunker Cheat Loader`,
 	namespace: `https://forum.sys32.dev/`,
 	icon: `https://y9x.github.io/webpack/libs/gg.gif`,
+	license: 'gpl-3.0',
 	version: 1.23,
 	match: [ 'https://krunker.io/*', 'https://*.browserfps.com/*' ],
 };
